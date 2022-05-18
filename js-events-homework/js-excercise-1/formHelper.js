8fc1c0a1c071c113f92d72f6c38d72ab3a044478
SaraMojsova <saramojs@gmail.com>
2022-05-18 20:12:48 +0200

    JS Events 2 - excercise 1 (with a little help)

---
+++ js-events-homework/js-excercise-1/formHelper.js
@@ -0,0 +1,106 @@
+function showMessage(input, message, isValid) {
+    const errorEl = input.nextElementSibling; // input -> p
+    errorEl.innerText = message;
+    input.className = isValid ? "success" : "error";
+    if(!isValid){
+        input.className="icons"
+    }
+    return isValid;
+  }
+  
+  function showError(input, message) {
+    return showMessage(input, message, false);
+  }
+  
+  function showSuccess(input) {
+    return showMessage(input, '', true);
+  }
+  
+  /**
+   * Validation functions
+   */
+  function hasValue(input, message) {
+    if (input.value.trim() === '') {
+      return showError(input, message);
+    }
+    return showSuccess(input);
+  }
+  
+  function validateEmail(input) {
+    if (!hasValue(input, 'Email is required')) {
+      return false;
+    }
+  
+    const emailRegex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+    const emailValue = input.value.trim();
+    if (!emailRegex.test(emailValue)) {
+      return showError(input, 'Invalid email address');
+    }
+    return true;
+  }
+  
+  function validatePassword(input) {
+    if (!hasValue(input, 'Password is required')) {
+      return false;
+    }
+  
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+  
+    const passwordValue = input.value.trim();
+    
+    if (!passwordRegex.test(passwordValue)) {
+      return showError(input, 'The password should contain an upper case, lower case and a number');
+    }
+    return true;
+  }
+// NEW CODE
+
+function validateEmailLength(input, min, max) {    
+    const inputEmail = input.value;    
+    const minLength = min;    
+    const maxLength = max;      
+    if (inputEmail.length !== 0 && inputEmail.length < minLength || inputEmail.length > maxLength) {      
+        return showError(input, 'Email must contain at 3-320 characters');    
+    }else {        
+        return true;    
+    }  
+}    
+function validatePasswordLength(input, min, max) {    
+     const inputPassword = input.value;    
+     const minLength = min;    
+     const maxLength = max; 
+     
+     let lengthPassword = inputPassword.length++
+     console.log(inputPassword)
+     console.log(lengthPassword)
+
+     if (inputPassword.length !== 0 && inputPassword.length < minLength || inputPassword.length > maxLength) {      
+         return showError(input, 'Password must contain 8-20 characters');    
+     }else {        
+        return true;    
+    }  
+}
+
+
+// THIS IS SOMETHING I TRIED AS A FIRST IDEA BUT DIDN'T SUCCEED
+
+// function passwordCharLength(input){
+    //     // const charLengthRegex=/^(?=.{7,20}$).*/;
+    //     // const charLengthRegex= /^\w{5,10}$/;
+    //     const passwordLength=input.value.trim().length
+    //     if(passwordLength<8 && passwordLength>20){
+    //         return showError(input, 'Password must contain 8-20 characters')
+    //     }
+    //     return true;
+    //   }
+
+    //   function emailCharLength(input){
+    //     // const charLengthRegex=/^{,320}$/;
+    //     const emailValue=input.value.trim().length
+    //     if(emailValue>320){
+    //         return showError(input, 'Email limit is 320 characters')
+    //     }
+    //     return true;
+    //   }
