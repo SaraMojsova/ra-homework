d4acdb425b0b743bf09aa42489c4ee8f4a61d014
SaraMojsova <saramojs@gmail.com>
2022-06-08 17:28:35 +0200

    second part of exercise completed

--- js-advanced-functions/fn-exercise-1.js
+++ js-advanced-functions/fn-exercise-1.js
@@ -1,65 +1,53 @@
 //1. part one
 let initialData=[200,500,300,1000]
 console.log('INITIAL DATA: ',initialData)
-let newArray=[]
 console.log('Part 1 of the exercise -------------')
 
 const incrementing=(array,padding)=>{
+    const newArray=[];
     for(i=0;i<array.length;i++){
-        array[i]=array[i]+padding
-        // console.log(array)
+        newArray.push(array[i]+padding)
     }
     // for(el of array){
-    //     el=el+padding
-    //     // console.log(el)
+    //     el=el+padding //remove this
+    //     newArray.push(el+padding) //same to this
+
     // }
     // the two for cycles do the same
-    newArray=array
     console.log('Incrementing function by 2',newArray)
 }
-incrementing(initialData,padding=2)
+incrementing(initialData,2)
 
-let newArray2=[]
-initialData=[200,500,300,1000]
 
 const decrementing=(array,padding)=>{
+    const newArray2=[]
     for(i=0;i<array.length;i++){
-        array[i]=array[i]-padding
-        // console.log(array)
+        newArray2.push(array[i]-padding)
     }
-    newArray2=array
     console.log('Decrementing function by 2',newArray2)
 }
-decrementing(initialData,padding=2)
+decrementing(initialData,2)
 
 //2.part two 
 //HOF
 console.log('Part 2 of the exercise --------------')
-let newArray3=[]
-initialData=[200,500,300,1000]
 
-const operationInc = (array,padding)=>{
-    for(i=0;i<array.length;i++){
-        array[i]=array[i]+padding
-    }
-    newArray3=array
-    return newArray3    
+const operationInc = (number,padding)=>{
+    return number+padding       
 }
 // 
-let newArray4=[]
-initialData=[200,500,300,1000]
-
-const operationDec = (array,padding)=>{
-    for(i=0;i<array.length;i++){
-        array[i]=array[i]-padding;
-        // console.log('initial data',initialData)
-    }
-    newArray4=array
-    return newArray4    
-}//is not working
-const main =(array,fn,padding)=>{
-    // array=initialData
-    console.log('HOF call (decrement) by 3',fn(array,padding))
+const operationDec = (number,padding)=>{
+    return number-padding    
 }
-main(initialData,operationInc,padding=3)
-main(initialData,operationDec,padding=3)
+const main =(array,fn,padding)=>{
+    const newArray=[]
+    for(const elem of array){
+        newArray.push(fn(elem,padding))
+    }
+    return newArray
+}
+const incrHOF=main(initialData,operationInc,3)
+const decrHOF=main(initialData,operationDec,3)
+console.log(incrHOF)
+console.log(decrHOF)
+// Where to print the message `HOF call (increment) by 3` and `HOF call (increment) by 3` ?
