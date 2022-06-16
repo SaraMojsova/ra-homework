6d6ed6384fc62008a7415857c2925499934f76a0
SaraMojsova <saramojs@gmail.com>
2022-06-16 16:57:35 +0200

    update

--- js-ajax/js-ajax-1/dom.js
+++ js-ajax/js-ajax-1/dom.js
@@ -1,21 +1,20 @@
 const cards=document.getElementById('cards')
-const p=document.createElement('div') //**** this one
 
 const buildUI=(post)=>{
     const {body,id,title,userId}=post 
-    // const p=`
-    //with this one ***** ->
+    const p=document.createElement('div')
+    p.classList.add('newPost')
     p.innerHTML=` 
     <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
-    <div class="card-header">Id: ${id} UserId: ${userId}</div>
+    <div class="card-header">Post Id: ${id} User Id: ${userId}</div>
     <div class="card-body">
-      <h5 class="card-title">Title:${title}</h5>
-      <p class="card-text">Body:${body}</p>
+      <h5 class="card-title">Title: ${title}</h5>
+      <p class="card-text">Body: ${body}</p>
       <button type="button" id="${id}" class="btn btn-light">Delete</button>
     </div>
   </div>
     `;
+
     
-    cards.insertAdjacentElement('afterend',p) 
-    // cards.append(p)
+    cards.insertAdjacentElement('beforeend',p) 
 }
