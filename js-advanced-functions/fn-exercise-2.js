080ca67e04476697c011de508d3600325d6834e5
SaraMojsova <saramojs@gmail.com>
2022-06-17 18:00:47 +0200

    update

--- js-advanced-functions/fn-exercise-2.js
+++ js-advanced-functions/fn-exercise-2.js
@@ -36,13 +36,13 @@ getUserData.call(user2, user2.field,`${user2.field??'isLoggedIn'} field doesn\'t
 console.log('---------------------------------')
 
 console.log('APPLY METHOD IMPLEMENTATION----------------------')
-const applyArray=[this.field, `${this.field??'isLoggedIn'} field doesn\'t exist on ${this?'user1':'user2'}`] // ${this?'user1':'user2'} not sure how to handle if it prints user1 or user2
-
+const applyArray=[this.field, `${this.field??'isLoggedIn'} field doesn\'t exist on user1`] // ${this?'user1':'user2'} not sure how to handle if it prints user1 or user2
+const applyArray2=[this.field, `${this.field??'isLoggedIn'} field doesn\'t exist on user2`]
 print.apply(user)
 getUserData.apply(user,applyArray)
 console.log('---------------------------------')
 print.apply(user2)
-getUserData.apply(user2,applyArray)
+getUserData.apply(user2,applyArray2)
 
 console.log('---------------------------------')
 console.log('BIND METHOD IMPLEMENTATION----------------------')
@@ -57,6 +57,6 @@ const getUserDataBind2=getUserDataB.bind(user2)
 
 printBind1();
 printBind2()
-getUserDataBind1(this.field, `${this.field??'isLoggedIn'} field doesn\'t exist on user1`)
-getUserDataBind2(this.field, `${this.field??'isLoggedIn'} field doesn\'t exist on user2`)
+getUserDataBind1(this.firstName, `${this.firstName??'isLoggedIn'} field doesn\'t exist on user1`)
+getUserDataBind2(this.firstName, `${this.firstName??'isLoggedIn'} field doesn\'t exist on user2`)
 
